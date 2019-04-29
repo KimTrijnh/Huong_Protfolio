dd07c7510ab8ffbf9c214f87b0ba09d2741f6d5e
Huong Trinh <trtrhuong@gmail.com>
2019-04-30 01:59:00 +0700

    adjust build

--- src/index.js
+++ src/index.js
@@ -4,8 +4,9 @@ import $ from 'jquery';
 import Popper from 'popper.js';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
 import "assets/css/blk-design-system-react.css";
-import App from "./App"
 import "assets/css/nucleo-icons.css";
+import App from "./App"
+
 
 
 
